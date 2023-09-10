931d9ba4f95275da6349985aa0069a4c2657a29a
anis191 <132251334+anis191@users.noreply.github.com>
2023-09-10 01:29:17 -0700

    Update login_form.php

--- login_form.php
+++ login_form.php
@@ -38,7 +38,7 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap" rel="stylesheet">
